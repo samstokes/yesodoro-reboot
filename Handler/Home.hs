53c4d69c9c042de12682ae536e84c980a2f617ed
Sam Stokes <me@samstokes.co.uk>
2012-05-06 01:22:09 -0700

    Auth on front page

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -2,8 +2,12 @@
 module Handler.Home where
 
 import Import
+import Yesod.Auth
 
 getHomeR :: Handler RepHtml
-getHomeR = defaultLayout $ do
-    setTitle "Welcome To Yesod!"
-    $(widgetFile "homepage")
+getHomeR = maybeAuthId >>= getHomeR' where
+  getHomeR' :: Maybe UserId -> Handler RepHtml
+  getHomeR' Nothing = defaultLayout $ do
+      setTitle "yesodoro"
+      $(widgetFile "homepage")
+  getHomeR' (Just _) = {-redirect-} undefined
