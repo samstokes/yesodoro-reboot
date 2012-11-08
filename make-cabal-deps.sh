4855b2695aa770f0faa88f2ed526166d709185ae
Sam Stokes <me@samstokes.co.uk>
2012-11-08 11:50:59 -0800

    fixup! script to ask

--- make-cabal-deps.sh
+++ make-cabal-deps.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cabal-dev buildopts | grep -A1 -- -package-id | grep -v '^-' | grep -v yesodoro-reboot | sort -u | sed 's/-[0-9a-f]\+$//;s/-\([0-9\.]\+\)$/ == \1/;1s/^/  /;2,$s/^/, /'
+cabal-dev buildopts | grep -A1 -- -package-id | grep -v '^-' | egrep -v 'base|yesodoro-reboot' | sort -u | sed 's/-[0-9a-f]\+$//;s/-\([0-9\.]\+\)$/ == \1/;1s/^/  /;2,$s/^/, /'
