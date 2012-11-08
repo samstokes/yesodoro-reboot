47e42f3f18b6168b4e14d9685a0df70d22c4bedb
Sam Stokes <me@samstokes.co.uk>
2012-11-08 13:06:16 -0800

    second attempt at deps script

--- make-cabal-deps.sh
+++ make-cabal-deps.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cabal-dev buildopts | grep -A1 -- -package-id | grep -v '^-' | egrep -v 'base|yesodoro-reboot' | sort -u | sed 's/-[0-9a-f]\+$//;s/-\([0-9\.]\+\)$/ == \1/;1s/^/  /;2,$s/^/, /'
+echo :quit | cabal-dev ghci 2>/dev/null | sed -n 's/^Loading package //p' | sed 's/ \.\.\. linking \.\.\. done\.$//;s/-\([0-9\.]\+\)$/ == \1/;1s/^/  /;2,$s/^/, /'
