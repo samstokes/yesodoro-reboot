132fdb08e6fdf9b223a8e204f4e05787924074bc
Sam Stokes <me@samstokes.co.uk>
2012-11-17 22:40:41 -0800

    Robustify expandy widget to special characters

--- Widget/Expandy.hs
+++ Widget/Expandy.hs
@@ -5,6 +5,7 @@ module Widget.Expandy
 
 import Prelude
 
+import Data.Aeson (toJSON)
 import Data.Text (Text)
 import Yesod (GWidget, addScriptEither, getYesod, newIdent)
 import Yesod.Default.Util (widgetFileNoReload)
