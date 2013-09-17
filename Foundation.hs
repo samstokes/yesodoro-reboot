95235bbb9f53d0c15a20b7e13a22743e806a8abe
Sam Stokes <me@samstokes.co.uk>
2013-09-17 00:28:01 -0700

    TODO WIP Make tasks sortable by dragging (TODO verify)

--- Foundation.hs
+++ Foundation.hs
@@ -128,6 +128,7 @@ instance Yesod App where
             addScriptEither $ eitherDev (StaticR js_angular_1_1_5_js) "//ajax.googleapis.com/ajax/libs/angularjs/1.1.5/angular.min.js"
             addScript $ StaticR js_showdown_js
             addScript $ StaticR js_angular_markdown_js
+            addScript $ StaticR $ js_angular_ui_sortable_js
             addScript $ StaticR js_expandy_js
             addStylesheet $ StaticR css_expandy_css
         hamletToRepHtml $(hamletFile "templates/default-layout-wrapper.hamlet")
