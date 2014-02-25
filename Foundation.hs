2a9304374e06f537b7f21fbc0f265f541fb34668
Sam Stokes <me@samstokes.co.uk>
2014-02-25 11:27:56 -0800

    Add app.models module

--- Foundation.hs
+++ Foundation.hs
@@ -135,6 +135,8 @@ instance Yesod App where
             addScript $ StaticR js_lib_expandy_js
             addStylesheet $ StaticR css_expandy_css
 
+            addScript $ StaticR js_app_models_js
+
             addScript $ StaticR js_app_services_js
             addScript $ StaticR js_app_services_Tasks_js
 
