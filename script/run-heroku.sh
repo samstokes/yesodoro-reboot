ac0376a68650fcfef65715d3def0ae9751c4cfaa
Sam Stokes <me@samstokes.co.uk>
2014-02-28 18:33:31 -0800

    Explain what the heck we're doing

--- script/run-heroku.sh
+++ script/run-heroku.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# Allows us to set the client session secret via a Heroku config variable
 if [ -n "$CLIENT_SESSION_KEY" ]; then
+  # Yesod's Web.ClientSession module already knows how to read the secret from a
+  # file, so we just write to the file configured in Foundation.hs (in
+  # makeSessionBackend)
   base64 --decode <<<$CLIENT_SESSION_KEY >config/client_session_key.aes
 fi
 
