54e27befd238cdd91c3558c92436cb75a85d72bc
Sam Stokes <me@samstokes.co.uk>
2014-05-11 02:56:55 -0700

    Work around PhantomJS not supporting Function.prototype.bind

---
+++ tests/jasmine/support/functionBindPolyfill.js
@@ -0,0 +1,31 @@
+/*
+ * PhantomJS doesn't support Function.prototype.bind:
+ * https://groups.google.com/d/msg/phantomjs/r0hPOmnCUpc/uxusqsl2LNoJ
+ *
+ * This polyfill should make our tests work.  It's taken from
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind#Compatibility
+ */
+
+if (!Function.prototype.bind) {
+  Function.prototype.bind = function (oThis) {
+    if (typeof this !== "function") {
+      // closest thing possible to the ECMAScript 5 internal IsCallable function
+      throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+    }
+
+    var aArgs = Array.prototype.slice.call(arguments, 1), 
+        fToBind = this, 
+        fNOP = function () {},
+        fBound = function () {
+          return fToBind.apply(this instanceof fNOP && oThis
+                                 ? this
+                                 : oThis,
+                               aArgs.concat(Array.prototype.slice.call(arguments)));
+        };
+
+    fNOP.prototype = this.prototype;
+    fBound.prototype = new fNOP();
+
+    return fBound;
+  };
+}
