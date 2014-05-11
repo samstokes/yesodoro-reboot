0a93065383211566d16c96ae53305fccd4c43bdf
Sam Stokes <me@samstokes.co.uk>
2014-05-11 14:44:22 -0700

    `bower install --save-dev jasmine-jquery`

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -14,9 +14,11 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+      'static/js/vendor/jquery-1.6.4.js',
       'static/js/vendor/angular-1.1.5.js',
       'static/js/lib/**/*.js',
       'static/js/app/**/*.js',
+      'bower_components/jasmine-jquery/lib/jasmine-jquery.js',
       'tests/jasmine/support/**/*.js',
       'tests/jasmine/**/*Spec.js'
     ],
