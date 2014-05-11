bcc6c93f483dde0be3e9e11a94372a417813e110
Sam Stokes <me@samstokes.co.uk>
2014-05-11 02:47:38 -0700

    Specdoc-style output for Jasmine specs

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -29,7 +29,7 @@ module.exports = function(config) {
 
     // test results reporter to use
     // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-    reporters: ['progress', 'coverage'],
+    reporters: ['spec', 'coverage'],
 
 
     coverageReporter: {
