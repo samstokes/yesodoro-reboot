53e7285ce2bf7fef595c06faa83f319a2f0b10c0
Sam Stokes <me@samstokes.co.uk>
2014-07-25 17:36:46 +0100

    Support adding tasks in new design

--- Handler/Templates.hs
+++ Handler/Templates.hs
@@ -7,6 +7,9 @@ import Util.Http
 import Util.Angular
 
 import qualified Data.ByteString.Lazy.Char8 as B
+import qualified Data.Char as Char
+import Data.Monoid ((<>))
+import Text.Blaze (Markup, toMarkup)
 import Text.Hamlet (hamletFile)
 
 
@@ -35,7 +38,17 @@ getTemplateTaskTrR = do
 
 
 getTemplateNewSettingsR = hamletToRepHtml $(hamletFile "templates/new-design/settings.hamlet")
-getTemplateNewTasksTodayR = hamletToRepHtml $(hamletFile "templates/new-design/tasks-today.hamlet")
+getTemplateNewTasksTodayR = do
+  Entity _ user <- requireNgAuth
+  withDigestEtag (featuresEtag user) $ do
+    let
+      features = userFeatureSettings user
+      has feature = hasFlag feature features
+      scheduleOptions = if has FeatureNonDailySchedules
+        then schedules
+        else filter (not . nonDaily) schedules
+    hamletToRepHtml $(hamletFile "templates/new-design/tasks-today.hamlet")
+
 getTemplateNewTasksLaterR = hamletToRepHtml $(hamletFile "templates/new-design/tasks-later.hamlet")
 getTemplateNewTasksDoneR = hamletToRepHtml $(hamletFile "templates/new-design/tasks-done.hamlet")
 getTemplateNewTaskR = hamletToRepHtml $(hamletFile "templates/new-design/task.hamlet")
@@ -45,3 +58,7 @@ featuresEtag user = return . B.pack . concat $ [
     show estimateOptions
   , show (userFeatures user)
   ]
+
+
+toScheduleId :: Schedule -> Markup
+toScheduleId = toMarkup . ("schedule-" <>) . map Char.toLower . show
