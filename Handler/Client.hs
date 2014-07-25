b2ee54365aeac074868850bb26f763ceac53e78c
Sam Stokes <me@samstokes.co.uk>
2014-07-25 17:28:00 +0100

    Specify controller and resolves per-state

--- Handler/Client.hs
+++ Handler/Client.hs
@@ -40,6 +40,15 @@ stateClientR StateTasksDone = NewTasksDoneR
 stateR :: State -> Route App
 stateR = ClientR . stateClientR
 
+stateController :: State -> Maybe Text
+stateController _ = Nothing
+
+stateResolves :: State -> [(Text, t -> Javascript)]
+stateResolves StateTasksToday = [
+    ("tasks", [julius|function (Tasks) { return Tasks.all(); }|])
+  ]
+stateResolves _ = []
+
 stateTemplateR :: State -> Route App
 stateTemplateR StateSettings = TemplateNewSettingsR
 stateTemplateR StateTasksToday = TemplateNewTasksTodayR
@@ -57,12 +66,18 @@ stateDeclJavascript :: State -> (Route App -> [param] -> Text) -> Javascript
 stateDeclJavascript state = [julius|
     .state(#{toJSON state}, {
       url: '@{stateR state}',
-      resolve: {
-        tasks: function (Tasks) { return Tasks.all(); }
-      },
+      ^{controllerDecl (stateController state)}
+      ^{resolvesDecl (stateResolves state)}
       templateUrl: '@{stateTemplateR state}'
     })
-|]
+|] where
+  controllerDecl (Just ctrl) = [julius|controller: #{toJSON ctrl},|]
+  controllerDecl Nothing = mempty
+  resolvesDecl [] = mempty
+  resolvesDecl resolves = [julius|resolve: {
+        ^{foldMap resolveDecl resolves}
+      },|]
+  resolveDecl (key, resolve) = [julius|#{key}: ^{resolve}|]
 
 statesDeclJavascript :: (Route App -> [param] -> Text) -> Text -> Javascript
 statesDeclJavascript render stateProvider = header <> foldMap decl states
