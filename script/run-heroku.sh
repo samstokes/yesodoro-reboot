c27cf3824ed04eb39bd59012a806e682268031f8
Sam Stokes <me@samstokes.co.uk>
2014-12-22 18:49:42 +0000

    Configure for Halcyon which actually installs the app binary

--- script/run-heroku.sh
+++ script/run-heroku.sh
@@ -8,4 +8,4 @@ if [ -n "$CLIENT_SESSION_KEY" ]; then
   base64 --decode <<<$CLIENT_SESSION_KEY >config/client_session_key.aes
 fi
 
-exec ./dist/build/yesodoro-reboot/yesodoro-reboot "$@"
+exec yesodoro-reboot "$@"
